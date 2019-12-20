619e4c532575b095fe2de9de39142ecddce5f18c
Samo Pogačnik <samo_pogacnik@t-2.net>
2019-12-20 17:41:36 +0100

    Changed project license to Apache License, Version 2.0.

--- include/evm/libevm.h
+++ include/evm/libevm.h
@@ -1,12 +1,22 @@
 /*
  * The public API of the event machine (libevm) library
  *
- * Copyright (C) 2014 Samo Pogacnik <samo_pogacnik@t-2.net>
- * All rights reserved.
+ * This file is part of the "evm" software project which is
+ * provided under the Apache license, Version 2.0.
  *
- * This file is part of the EVM software project.
- * This file is provided under the terms of the BSD 3-Clause license,
- * available in the LICENSE file of the EVM software project.
+ *  Copyright 2019 Samo Pogacnik <samo_pogacnik@t-2.net>
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
 */
 
 #ifndef libevm_h
